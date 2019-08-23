45e91787bbeb624ce61f22a400ad872b7ec35e64
qiaoyi <qiaoyi@meituan.com>
2019-08-23 15:31:03 +0800

    leetcode

---
+++ src/main/java/leecode/array/_26_remove_delplicates_from_sorted_array.java
@@ -0,0 +1,20 @@
+package leecode.array;
+
+/**
+ * @author: qiaoyi
+ * @edit:
+ * @created:2019/8/23
+ */
+public class _26_remove_delplicates_from_sorted_array {
+
+    public int removeDuplicates(int[] nums) {
+        int pPosition = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[pPosition] != nums[i]) {
+                nums[pPosition + 1] = nums[i];
+                pPosition++;
+            }
+        }
+        return pPosition + 1;
+    }
+}
