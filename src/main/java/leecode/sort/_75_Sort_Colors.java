793ae9694244a8cd24f66f5e1a90cfb07b4056bb
KyleQiao1992 <chocolatman1992@gmail.com>
2019-10-03 21:12:41 +0800

    75

---
+++ src/main/java/leecode/sort/_75_Sort_Colors.java
@@ -0,0 +1,4 @@
+package leecode.sort;
+
+public class _75_Sort_Colors {
+}
---
+++ src/main/java/practice/QuickSort2.java
@@ -0,0 +1,38 @@
+package practice;
+
+public class QuickSort2 {
+
+    private static int partitionSimple(int[] arr, int l, int r) {
+        int v = arr[l];
+        int j = l;
+        for (int i = l + 1; i <= r; i++) {
+            if (arr[i] < v) {
+                swap(arr, i, j);
+                j++;
+            }
+        }
+        swap(arr, l, j);
+        return j;
+    }
+
+    private static void sortSimple(int[] arr, int l, int r) {
+        if (l >= r) {
+            return;
+        }
+
+        int p = partitionSimple(arr, l, r);
+        sortSimple(arr, 0, p);
+        sortSimple(arr, p + 1, r);
+    }
+
+    public static void sortSimple(int[] arr) {
+        int length = arr.length;
+        sortSimple(arr, 0, length - 1);
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int t = arr[i];
+        arr[i] = arr[j];
+        arr[j] = t;
+    }
+}
