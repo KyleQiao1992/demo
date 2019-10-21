74f68c63e64eb79a929a7d7d666970b95b04e9bb
KyleQiao1992 <chocolatman1992@gmail.com>
2019-10-21 15:27:04 +0800

    345

---
+++ src/main/java/leecode/array/_11_container_with_most_water.java
@@ -0,0 +1,5 @@
+package leecode.array;
+
+public class _11_container_with_most_water {
+
+}
--- src/main/java/leecode/array/_345_reverse_vowels_of_a_string.java
+++ src/main/java/leecode/array/_345_reverse_vowels_of_a_string.java
@@ -1,4 +1,42 @@
 package leecode.array;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class _345_reverse_vowels_of_a_string {
+
+    public static String reverseVowels(String s) {
+        if (s == null || s.isEmpty()) {
+            return s;
+        }
+        List<String> vowelsList = Arrays.asList("a", "e", "i", "o", "u", "A", "E", "I", "O", "U");
+        char[] sChar = s.toCharArray();
+        int l = 0;
+        int r = sChar.length - 1;
+
+        while (l <= r) {
+            if (!vowelsList.contains(String.valueOf(sChar[l]))) {
+                l++;
+                continue;
+            }
+
+            if (!vowelsList.contains(String.valueOf(sChar[r]))) {
+                r--;
+                continue;
+            }
+            swap(sChar, l++, r--);
+        }
+        return String.valueOf(sChar);
+    }
+
+    private static void swap(char[] arr, int i, int j) {
+        char t = arr[i];
+        arr[i] = arr[j];
+        arr[j] = t;
+    }
+
+    public static void main(String[] args) {
+        String testStr = "leetcode";
+        String result = reverseVowels(testStr);
+    }
 }
