4bc67f15a7bdc34160b2193be64bbe226ce9bd2a
KyleQiao1992 <chocolatman1992@gmail.com>
2019-12-06 18:47:22 +0800

    92

--- src/main/java/leecode/linked_list/_92_reverse_linked_list_II.java
+++ src/main/java/leecode/linked_list/_92_reverse_linked_list_II.java
@@ -1,8 +1,51 @@
 package leecode.linked_list;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class _92_reverse_linked_list_II {
 
-    public ListNode reverseBetween(ListNode head, int m, int n) {
-        return head;
+    public static ListNode reverseBetween(ListNode head, int m, int n) {
+        //如果头结点只有一个元素直接返回head
+        if (head.next == null) {
+            return head;
+        }
+        ListNode dummyHead = new ListNode(-1);
+        dummyHead.next = head;
+        ListNode beginNode = null;
+        ListNode endNode = null;
+        //循环一次找到beginNode和endNode
+        int i = 1;
+        ListNode cur = head;
+        while (i <= n) {
+            if (i == m) {
+                beginNode = cur;
+            }
+            if (i == n) {
+                endNode = cur;
+            }
+            cur = cur.next;
+            i++;
+        }
+
+        ListNode pre = null;
+        cur = beginNode;
+        while (cur != null && endNode != null && cur != endNode.next) {
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            if (cur != endNode) {
+                cur = next;
+            }
+        }
+        head.next = cur;
+        return head.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode testNodeList = ListNode.buildListNode(Arrays.asList(1, 2, 3, 4, 5));
+        reverseBetween(testNodeList, 2, 4);
+        ListNode.printListNode(testNodeList);
     }
 }
