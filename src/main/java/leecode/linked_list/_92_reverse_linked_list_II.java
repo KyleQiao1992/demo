1db8328b5dcbfe948ff62b4f3d0cd682ca2da4bf
KyleQiao1992 <chocolatman1992@gmail.com>
2019-12-06 19:52:37 +0800

    update

--- src/main/java/leecode/linked_list/_92_reverse_linked_list_II.java
+++ src/main/java/leecode/linked_list/_92_reverse_linked_list_II.java
@@ -18,8 +18,9 @@ public class _92_reverse_linked_list_II {
         //循环一次找到beginNode和endNode
         int i = 1;
         ListNode cur = head;
+
         while (i <= n) {
-            if (i == m) {
+            if (i == m - 1) {
                 beginNode = cur;
             }
             if (i == n) {
@@ -30,21 +31,23 @@ public class _92_reverse_linked_list_II {
         }
 
         ListNode pre = null;
-        cur = beginNode;
-        while (cur != null && endNode != null && cur != endNode.next) {
+        cur = beginNode.next;
+        i = 0;
+        while (cur != null && i <= n - m) {
             ListNode next = cur.next;
             cur.next = pre;
             pre = cur;
-            if (cur != endNode) {
-                cur = next;
-            }
+            cur = next;
+            i++;
         }
+        beginNode.next = pre;
+       
         head.next = cur;
         return head.next;
     }
 
     public static void main(String[] args) {
-        ListNode testNodeList = ListNode.buildListNode(Arrays.asList(1, 2, 3, 4, 5));
+        ListNode testNodeList = ListNode.buildListNode(Arrays.asList(1, 2, 3, 4, 5, 6));
         reverseBetween(testNodeList, 2, 4);
         ListNode.printListNode(testNodeList);
     }
