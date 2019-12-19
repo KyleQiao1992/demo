43c0e960581d9a3ba31d82e895cfa91c471deb87
KyleQiao1992 <chocolatman1992@gmail.com>
2019-12-19 15:23:51 +0800

    19

--- src/main/java/leecode/linked_list/_19_remove_nth_node_from_end_of_list.java
+++ src/main/java/leecode/linked_list/_19_remove_nth_node_from_end_of_list.java
@@ -8,25 +8,47 @@ public class _19_remove_nth_node_from_end_of_list {
         if (head == null) {
             return null;
         }
-        if (head.next == null && n == 1) {
-            return null;
-        }
-        int lenght = 0;
-        ListNode cur = head;
+        ListNode dummyHead = new ListNode(-1);
+        dummyHead.next = head;
+        int length = -1;
+        ListNode cur = dummyHead;
         while (cur != null) {
-            lenght++;
+            length++;
             cur = cur.next;
         }
-        cur = head;
-        for (int i = 0; i < lenght - n - 1; i++) {
+        cur = dummyHead;
+        int counter = Math.max((length - n), 0);
+        for (int i = 0; i < counter; i++) {
             cur = cur.next;
         }
         cur.next = cur.next.next;
-        return head;
+        return dummyHead.next;
+    }
+
+    //way2 双指针
+    public static ListNode removeNthFromEnd2(ListNode head, int n) {
+        if (head == null || n < 0) {
+            return null;
+        }
+        ListNode dummyHead = new ListNode(-1);
+        dummyHead.next = head;
+        ListNode frontNode = dummyHead;
+        ListNode endNode = dummyHead;
+        for (int i = 0; i < n + 1; i++) {
+            endNode = endNode.next;
+        }
+        while (endNode != null) {
+            frontNode = frontNode.next;
+            endNode = endNode.next;
+        }
+        ListNode delNode = frontNode.next;
+        frontNode.next = delNode.next;
+        return dummyHead.next;
     }
 
     public static void main(String[] args) {
         ListNode head = ListNode.buildListNode(Arrays.asList(1));
-        removeNthFromEnd1(head, 1);
+//        removeNthFromEnd1(head, 2);
+        removeNthFromEnd2(head, 1);
     }
 }
