221438b00cb8f078a8c112bdd8649f884dd1adba
KyleQiao1992 <chocolatman1992@gmail.com>
2019-12-20 16:06:36 +0800

    update

--- src/main/java/leecode/linked_list/_25_reverse_nodes_in_k_group.java
+++ src/main/java/leecode/linked_list/_25_reverse_nodes_in_k_group.java
@@ -1,8 +1,13 @@
 package leecode.linked_list;
 
+import java.util.Arrays;
+import java.util.Stack;
+
 public class _25_reverse_nodes_in_k_group {
 
-    public ListNode reverseKGroup(ListNode head, int k) {
+    //way1 using auxiliary space
+    public static ListNode reverseKGroup(ListNode head, int k) {
         return null;
     }
+
 }
