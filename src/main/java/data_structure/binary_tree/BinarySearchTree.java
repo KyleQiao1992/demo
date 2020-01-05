c778b087202c177a7b6e678714394d8582655292
KyleQiao1992 <chocolatman1992@gmail.com>
2020-01-05 21:31:36 +0800

    bt tree

--- src/main/java/data_structure/binary_tree/BinarySearchTree.java
+++ src/main/java/data_structure/binary_tree/BinarySearchTree.java
@@ -1,5 +1,9 @@
 package data_structure.binary_tree;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 public class BinarySearchTree<E extends Comparable<E>> {
     private class Node {
         public E e;
@@ -81,6 +85,99 @@ public class BinarySearchTree<E extends Comparable<E>> {
         System.out.println(node.e);
         preOrder(node.left);
         preOrder(node.right);
+    }
+
+    //中序遍历二叉树
+    public void inOrder() {
+        inOrder(root);
+    }
+
+    // 中序遍历以node为根的二分搜索树
+    private void inOrder(Node node) {
+        if (node == null)
+            return;
+
+        inOrder(node.left);
+        System.out.println(node.e);
+        inOrder(node.right);
+    }
+
+    //二分搜索树的后续遍历
+    public void postOrder() {
+        postOrder(root);
+    }
+
+    //后序遍历以node为根的二分搜索树
+    private void postOrder(Node node) {
+        if (node == null)
+            return;
+
+        postOrder(node.left);
+        postOrder(node.right);
+        System.out.println(node.e);
+    }
+
+    //二分搜索树的非递归前序遍历
+    public void preOrderNR() {
+        Stack<Node> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            Node cur = stack.pop();
+            System.out.println(cur.e);
+
+            if (cur.right != null) {
+                stack.push(cur.right);
+            }
+            if (cur.left != null) {
+                stack.push(cur.left);
+            }
+        }
+    }
+
+    //二分搜索树的层序遍历
+    public void levelOrder() {
+        Queue<Node> queue = new LinkedList();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            Node cur = queue.remove();
+            System.out.println(cur.e);
+            if (cur.left != null) {
+                queue.add(cur.left);
+            }
+            if (cur.right != null) {
+                queue.add(cur.right);
+            }
+        }
+    }
+
+    //寻找二分搜索树的最小元素
+    public E minmum() {
+        if (size == 0) {
+            throw new IllegalArgumentException("BinarySearchTree is empty");
+        }
+        return minmum(root).e;
+    }
+
+    private Node minmum(Node node) {
+        if (node.left == null) {
+            return node;
+        }
+        return minmum(node.left);
+    }
+
+    //寻找二分搜索树的最大元素
+    public E maximum() {
+        if (size == 0) {
+            throw new IllegalArgumentException("BinarySearchTree is empty");
+        }
+        return maximum(root).e;
+    }
+
+    private Node maximum(Node node) {
+        if (node.right == null) {
+            return node;
+        }
+        return maximum(node.right);
     }
 
     @Override
