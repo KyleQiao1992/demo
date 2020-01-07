39c3fe6fc2d3dd7c67cab546d5405f7d8c9931eb
KyleQiao1992 <chocolatman1992@gmail.com>
2020-01-07 17:21:54 +0800

    144

--- src/main/java/leecode/bt_search_tree/_144_binary_tree_preorder_traversal.java
+++ src/main/java/leecode/bt_search_tree/_144_binary_tree_preorder_traversal.java
@@ -2,6 +2,7 @@ package leecode.bt_search_tree;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class _144_binary_tree_preorder_traversal {
 
@@ -15,18 +16,62 @@ public class _144_binary_tree_preorder_traversal {
         }
     }
 
-    public List<Integer> preorderTraversal(TreeNode root) {
+    /**
+     * 方法1 常规递归方式
+     *
+     * @param root
+     * @return
+     */
+    public List<Integer> preOrderTraversal(TreeNode root) {
 
         List<Integer> res = new ArrayList<Integer>();
-        preorderTraversal(root, res);
+        preOrderTraversal(root, res);
         return res;
     }
 
-    private void preorderTraversal(TreeNode node, List<Integer> list){
-        if(node != null){
+    private void preOrderTraversal(TreeNode node, List<Integer> list) {
+        if (node != null) {
             list.add(node.val);
-            preorderTraversal(node.left, list);
-            preorderTraversal(node.right, list);
+            preOrderTraversal(node.left, list);
+            preOrderTraversal(node.right, list);
         }
+    }
+
+    /**
+     * 方法2： 非递归的方式实现前序遍历
+     */
+    private class Command {
+        String s;   // go, print
+        TreeNode node;
+
+        Command(String s, TreeNode node) {
+            this.s = s;
+            this.node = node;
+        }
+    }
+
+    public List<Integer> preOrderTraversal2(TreeNode root) {
+
+        ArrayList<Integer> res = new ArrayList<Integer>();
+        if (root == null)
+            return res;
+
+        Stack<Command> stack = new Stack<Command>();
+        stack.push(new Command("go", root));
+        while (!stack.empty()) {
+            Command command = stack.pop();
+
+            if (command.s.equals("print"))
+                res.add(command.node.val);
+            else {
+                assert command.s.equals("go");
+                if (command.node.right != null)
+                    stack.push(new Command("go", command.node.right));
+                if (command.node.left != null)
+                    stack.push(new Command("go", command.node.left));
+                stack.push(new Command("print", command.node));
+            }
+        }
+        return res;
     }
 }
