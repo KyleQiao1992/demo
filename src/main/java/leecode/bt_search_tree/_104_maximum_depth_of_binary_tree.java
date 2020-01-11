ebe361fe88d9470ecc54ac87efb384437e508683
KyleQiao1992 <chocolatman1992@gmail.com>
2020-01-11 11:13:18 +0800

    104

---
+++ src/main/java/leecode/bt_search_tree/_104_maximum_depth_of_binary_tree.java
@@ -0,0 +1,24 @@
+package leecode.bt_search_tree;
+
+public class _104_maximum_depth_of_binary_tree {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int leftMaxDepth = maxDepth(root.left);
+        int rightMaxDepth = maxDepth(root.right);
+        return Math.max(leftMaxDepth, rightMaxDepth) + 1;
+    }
+}
