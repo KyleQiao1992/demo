8cd9a70af208f43c46ebd3cc306ab232693de8dd
KyleQiao1992 <chocolatman1992@gmail.com>
2020-01-13 09:51:02 +0800

    update

---
+++ src/main/java/leecode/bt_search_tree/_112_path_sum.java
@@ -0,0 +1,24 @@
+package leecode.bt_search_tree;
+
+public class _112_path_sum {
+
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) {
+            return false;
+        }
+
+        if (root.left == null && root.right == null) {
+            return root.val == sum;
+        }
+
+        if (hasPathSum(root.left, sum - root.val)) {
+            return true;
+
+        }
+        if (hasPathSum(root.right, sum - root.val)) {
+            return true;
+        }
+
+        return false;
+    }
+}
