ff199a971b13920859e93f995b093ee39179ef19
KyleQiao1992 <chocolatman1992@gmail.com>
2020-01-24 21:45:11 +0800

    804

---
+++ src/main/java/leecode/set/_804_unique_morse_code_words.java
@@ -0,0 +1,31 @@
+package leecode.set;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class _804_unique_morse_code_words {
+
+    public static int uniqueMorseRepresentations(String[] words) {
+        String[] morseList = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."};
+        if (words == null || words.length == 0) {
+            return 0;
+        }
+        Set<String> result = new HashSet<>();
+        for (String word : words) {
+            StringBuilder morse = new StringBuilder();
+            for (int i = 0; i < word.length(); i++) {
+                String tmp = morseList[word.charAt(i) - 'a'];
+                morse.append(tmp);
+            }
+            result.add(morse.toString());
+        }
+        return result.size();
+    }
+
+    public static void main(String[] args) {
+        String[] words = {"gin", "zen", "gig", "msg"};
+        int result = uniqueMorseRepresentations(words);
+    }
+
+}
