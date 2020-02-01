d79f0d5a4aa9cf256208f259f165b8e50d90aaab
KyleQiao1992 <chocolatman1992@gmail.com>
2020-02-01 21:18:48 +0800

    update

---
+++ src/main/java/leecode/heap_priority_queue/_top_k_frequency_element_2.java
@@ -0,0 +1,49 @@
+package leecode.heap_priority_queue;
+
+import java.util.*;
+
+public class _top_k_frequency_element_2 {
+    private class Freq {
+
+        public int e, freq;
+
+        public Freq(int e, int freq) {
+            this.e = e;
+            this.freq = freq;
+        }
+    }
+
+    private class FreqComparator implements Comparator<Freq> {
+
+        @Override
+        public int compare(Freq a, Freq b) {
+            return a.freq - b.freq;
+        }
+    }
+
+    public List<Integer> topKFrequent(int[] nums, int k) {
+
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        for (int num : nums) {
+            if (map.containsKey(num))
+                map.put(num, map.get(num) + 1);
+            else
+                map.put(num, 1);
+        }
+
+        PriorityQueue<Freq> pq = new PriorityQueue<>(new FreqComparator());
+        for (int key : map.keySet()) {
+            if (pq.size() < k)
+                pq.add(new Freq(key, map.get(key)));
+            else if (map.get(key) > pq.peek().freq) {
+                pq.remove();
+                pq.add(new Freq(key, map.get(key)));
+            }
+        }
+
+        LinkedList<Integer> res = new LinkedList<>();
+        while (!pq.isEmpty())
+            res.add(pq.remove().e);
+        return res;
+    }
+}
---
+++ src/main/java/leecode/set_map/_16_3sum_closest.java
@@ -0,0 +1,9 @@
+package leecode.set_map;
+
+import java.util.Arrays;
+
+public class _16_3sum_closest {
+    public int threeSumClosest(int[] nums, int target) {
+        return 0;
+    }
+}
