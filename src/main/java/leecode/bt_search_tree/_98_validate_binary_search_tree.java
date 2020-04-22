ae38e7857ea1ef902abd5f37696ef419c2cea9dd
KyleQiao1992 <chocolatman1992@gmail.com>
2020-04-22 16:07:14 +0800

    update

--- src/main/java/leecode/bt_search_tree/_98_validate_binary_search_tree.java
+++ src/main/java/leecode/bt_search_tree/_98_validate_binary_search_tree.java
@@ -1,5 +1,9 @@
 package leecode.bt_search_tree;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class _98_validate_binary_search_tree {
     /**
      *  注意这种写法是错误的,因为二分搜索树的定义为：二分搜索树的每个节点大于其左子树的所有节点的值，小于其右子树所有节点的值
@@ -49,8 +53,37 @@ public class _98_validate_binary_search_tree {
         return isValidBST(root.left, min, root) && isValidBST(root.right, root, max);
     }
 
-    public boolean isValidBST2(TreeNode root) {
-
+    /**
+     * 中序遍历方法
+     *
+     * @param root
+     * @return
+     */
+    public static boolean isValidBST2(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        List<Integer> list = new ArrayList<>();
+        inOrder(root, list);
+        for (int i = 0; i + 1 < list.size(); i++) {
+            if (list.get(i) >= list.get(i + 1)) {
+                return false;
+            }
+        }
+        return true;
     }
 
+    private static void inOrder(TreeNode treeNode, List<Integer> list) {
+        if (treeNode != null) {
+            inOrder(treeNode.left, list);
+            list.add(treeNode.val);
+            inOrder(treeNode.right, list);
+        }
+    }
+
+    public static void main(String[] args) {
+        Integer[] list = {1, 1};
+        TreeNode treeNode = TreeNode.constructTree(list);
+        boolean result = isValidBST2(treeNode);
+    }
 }
