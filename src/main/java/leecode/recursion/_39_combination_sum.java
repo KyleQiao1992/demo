ec4596f2e6ef29055a575809bdcdeca587d71cd5
KyleQiao1992 <chocolatman1992@gmail.com>
2020-07-14 19:47:41 +0800

    up

--- src/main/java/leecode/recursion/_39_combination_sum.java
+++ src/main/java/leecode/recursion/_39_combination_sum.java
@@ -1,5 +1,51 @@
 package leecode.recursion;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
 public class _39_combination_sum {
+
+    List<List<Integer>> res = new ArrayList<>();
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        if (candidates == null || candidates.length == 0) {
+            return res;
+        }
+        Arrays.sort(candidates);
+        dfs(candidates, 0, new LinkedList<>(), target);
+        return res;
+    }
+
+    private void dfs(int[] candidates, int index, LinkedList<Integer> p, int target) {
+        Integer sum = 0;
+        for (Integer x : p) {
+            sum += x;
+        }
+        if (sum == target) {
+            res.add((List<Integer>) p.clone());
+            return;
+        }
+        if (sum > target) {
+            return;
+        }
+
+        for (int i = index; i < candidates.length; i++) {
+            if (i != index && candidates[i] == candidates[i - 1]) {
+                continue;
+            }
+            p.add(candidates[i]);
+            dfs(candidates, 0, p, target);
+            p.removeLast();
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] a = {2, 3, 6, 7};
+        _39_combination_sum f = new _39_combination_sum();
+        List<List<Integer>> res = f.combinationSum(a, 7);
+        System.out.println(res);
+    }
 
 }
--- src/main/java/leecode/recursion/_40_combination_sum_ii.java
+++ src/main/java/leecode/recursion/_40_combination_sum_ii.java
@@ -1,6 +1,9 @@
 package leecode.recursion;
 
+import data_structure.list.Array;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -11,10 +14,35 @@ public class _40_combination_sum_ii {
         if (candidates == null || candidates.length == 0) {
             return res;
         }
+        Arrays.asList(candidates);
+        dfs(candidates, 0, new LinkedList<>(), target);
         return res;
     }
 
     private void dfs(int[] candidates, int index, LinkedList<Integer> p, int target) {
+        Integer sum = 0;
+        for (Integer x : p) {
+            sum += x;
+        }
+        if (sum == target) {
+            res.add((List<Integer>) p.clone());
+            return;
+        }
+        if (sum > target) {
+            return;
+        }
 
+        for (int i = index; i < candidates.length; i++) {
+            p.add(candidates[i]);
+            dfs(candidates, i + 1, p, target);
+            p.removeLast();
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] ca = {10, 1, 2, 7, 6, 1, 5};
+        _40_combination_sum_ii f = new _40_combination_sum_ii();
+        List<List<Integer>> res = f.combinationSum2(ca, 8);
+        System.out.println(res);
     }
 }
