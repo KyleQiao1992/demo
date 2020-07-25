f9d63f84dbf727bb0fa744b07ad665dd9f511982
KyleQiao1992 <chocolatman1992@gmail.com>
2020-07-25 15:57:11 +0800

    up

---
+++ src/main/java/leecode/recursion/_200_numbers_of_islands.java
@@ -0,0 +1,69 @@
+package leecode.recursion;
+
+public class _200_numbers_of_islands {
+    private int d[][] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    private int m, n;
+    private boolean visited[][];
+
+    public int numIslands(char[][] grid) {
+
+        if (grid == null || grid.length == 0 || grid[0].length == 0)
+            return 0;
+
+        m = grid.length;
+        n = grid[0].length;
+
+        visited = new boolean[m][n];
+
+        int res = 0;
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+                if (grid[i][j] == '1' && !visited[i][j]) {
+                    dfs(grid, i, j);
+                    res++;
+                }
+
+        return res;
+    }
+
+    private void dfs(char[][] grid, int x, int y) {
+
+        //assert(inArea(x,y));
+        visited[x][y] = true;
+        for (int i = 0; i < 4; i++) {
+            int newx = x + d[i][0];
+            int newy = y + d[i][1];
+            if (inArea(newx, newy) && !visited[newx][newy] && grid[newx][newy] == '1')
+                dfs(grid, newx, newy);
+        }
+
+        return;
+    }
+
+    private boolean inArea(int x, int y) {
+        return x >= 0 && x < m && y >= 0 && y < n;
+    }
+
+    public static void main(String[] args) {
+
+        char grid1[][] = {
+                {'1', '1', '1', '1', '0'},
+                {'1', '1', '0', '1', '0'},
+                {'1', '1', '0', '0', '0'},
+                {'0', '0', '0', '0', '0'}
+        };
+        System.out.println((new _200_numbers_of_islands()).numIslands(grid1));
+        // 1
+
+        // ---
+
+        char grid2[][] = {
+                {'1', '1', '0', '0', '0'},
+                {'1', '1', '0', '0', '0'},
+                {'0', '0', '1', '0', '0'},
+                {'0', '0', '0', '1', '1'}
+        };
+        System.out.println((new _200_numbers_of_islands()).numIslands(grid2));
+        // 3
+    }
+}
