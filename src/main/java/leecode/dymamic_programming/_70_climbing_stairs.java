aeb81421420b9c2eae6dbb3e0c914fc18cc396de
KyleQiao1992 <chocolatman1992@gmail.com>
2020-07-26 14:00:35 +0800

    up

---
+++ src/main/java/leecode/dymamic_programming/_70_climbing_stairs.java
@@ -0,0 +1,18 @@
+package leecode.dymamic_programming;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class _70_climbing_stairs {
+
+    public int climbStairs(int n) {
+        Map<Integer, Integer> memo = new HashMap<>();
+        memo.put(0, 1);
+        memo.put(1, 1);
+
+        for (int i = 2; i <= n; i++) {
+            memo.put(i, memo.get(i - 1) + memo.get(i - 2));
+        }
+        return memo.get(n);
+    }
+}
---
+++ src/main/java/leecode/recursion/_70_climbing_stairs2.java
@@ -0,0 +1,31 @@
+package leecode.recursion;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class _70_climbing_stairs2 {
+    public Map<Integer, Integer> memo = new HashMap<>();
+
+    public int climbStairs(int n) {
+        return recursion(n);
+    }
+
+    private int recursion(int n) {
+
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
+        if (!memo.containsKey(n)) {
+            memo.put(n, recursion(n - 1) + recursion(n - 2));
+        }
+        return memo.get(n);
+    }
+
+    public static void main(String[] args) {
+        _70_climbing_stairs2 f = new _70_climbing_stairs2();
+        Integer res = f.climbStairs(45);
+    }
+}
