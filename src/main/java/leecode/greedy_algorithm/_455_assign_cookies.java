d8a024088dc09d0040ada42d8b732f62e35d894a
KyleQiao1992 <chocolatman1992@gmail.com>
2020-10-14 19:17:05 +0800

    new

---
+++ src/main/java/leecode/greedy_algorithm/_455_assign_cookies.java
@@ -0,0 +1,21 @@
+package leecode.greedy_algorithm;
+
+import java.util.Arrays;
+
+public class _455_assign_cookies {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int gi = g.length - 1, si = s.length - 1;
+        int res = 0;
+        while (gi >= 0 && si >= 0) {
+            if (s[si] >= g[gi]) {
+                res++;
+                si--;
+            }
+            gi--;
+        }
+        return res;
+    }
+
+}
