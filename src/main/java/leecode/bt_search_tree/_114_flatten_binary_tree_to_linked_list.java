d3f1ce25bba435863e5c48a8e33fd1e2f1c2924d
KyleQiao1992 <chocolatman1992@gmail.com>
2020-10-16 15:39:22 +0800

    add 114

---
+++ src/main/java/leecode/bt_search_tree/_114_flatten_binary_tree_to_linked_list.java
@@ -0,0 +1,10 @@
+package leecode.bt_search_tree;
+
+public class _114_flatten_binary_tree_to_linked_list {
+
+    public void flatten(TreeNode root) {
+
+    }
+
+    
+}
---
+++ src/main/java/leecode/bt_search_tree/no116/_116_populating_next_right_pointers_in_each_node_1.java
@@ -0,0 +1,41 @@
+package leecode.bt_search_tree.no116;
+
+public class _116_populating_next_right_pointers_in_each_node_1 {
+    class Node {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node next;
+
+        public Node() {
+        }
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, Node _left, Node _right, Node _next) {
+            val = _val;
+            left = _left;
+            right = _right;
+            next = _next;
+        }
+    }
+
+    Node connect(Node root) {
+        if (root == null) {
+            return root;
+        }
+        if (root.left != null) {
+            root.left.next = root.right;
+        }
+
+        if (root.right != null) {
+            root.right.next = root.next != null ? root.next.left : null;
+        }
+        connect(root.left);
+        connect(root.right);
+
+        return root;
+    }
+}
